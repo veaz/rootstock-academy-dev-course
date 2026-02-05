6b83cd491cc2eff7f3f76afdcfb51f1a2903b814
Victor Aguilar <veaz.24@gmail.com>
2026-02-05 22:22:48 +0100

    contract 02 done

--- module3/contracts/02-primitive-variables.sol
+++ module3/contracts/02-primitive-variables.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.0;
 
 contract Cars {
 
-    ___ ___ isBlack;
-    ___ ___ doors;
-    ___ ___ owner;
+    bool public isBlack;
+    uint256 public doors;
+    address public owner;
 
     constructor() {
     }
