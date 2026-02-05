1452a7b6c917380046b140abbe9ba9cd18141f7a
Victor Aguilar <veaz.24@gmail.com>
2026-02-05 22:53:52 +0100

    contract 10 done

--- module3/contracts/10-event-logs.sol
+++ module3/contracts/10-event-logs.sol
@@ -1,10 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 contract Cars {
 
     enum CarStatus { driving, parked }
 
-    ___ CarHonk(_________);
+    event CarHonk(uint256 indexed carId);
 
     struct Car {
         bytes3 colour;
@@ -59,9 +60,9 @@ contract Cars {
         uint256 carId
     )
         public
-        _________
+        onlyOwner(carId)
     {
-        ___ CarHonk(___);
+        emit CarHonk(carId);
     }
 
     modifier onlyOwner(
