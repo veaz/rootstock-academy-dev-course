a84e7f8d137b11d8625ce953311ff216cb7358fb
Victor Aguilar <veaz.24@gmail.com>
2026-02-14 14:14:52 +0100

    test 01-basic done

--- module4/test/01-basic-deployment.ts
+++ module4/test/01-basic-deployment.ts
@@ -5,46 +5,46 @@ describe("01-basic-deployment: Testing Contract Deployment", function () {
   
   it("should deploy the Cars contract successfully", async function () {
     // Get contract factory
-    const Cars = await ethers.___("Cars");
+    const Cars = await ethers.getContractFactory("Cars");
     
     // Deploy the contract
-    const cars = await Cars.___();
-    await cars.___();
-    
+    const cars = await Cars.deploy();
+    await cars.waitForDeployment();
+
     // Verify the contract address exists
-    const address = await cars.___();
-    ___(address).to.be.a("___");
-    ___(address).to.match(/^0x[a-fA-F0-9]{40}$/);
+    const address = await cars.getAddress();
+    expect(address).to.be.a("string");
+    expect(address).to.match(/^0x[a-fA-F0-9]{40}$/);
   });
 
   it("should initialize numCars to 0", async function () {
-    const Cars = await ethers.___("Cars");
-    const cars = await Cars.___;
-    await cars.___();
+    const Cars = await ethers.getContractFactory("Cars");
+    const cars = await Cars.deploy();
+    await cars.waitForDeployment();
     
     // Read the numCars state variable
-    const numCars = await cars.___();
+    const numCars = await cars.numCars();
     
     // Verify it's initialized to 0
-    ___(numCars).to.equal(___);
+    expect(numCars).to.equal(0);
   });
 
   it("should have a public numCars variable", async function () {
-    const Cars = await ethers.___("Cars");
-    const cars = await Cars.___;
-    await cars.___();
+    const Cars = await ethers.getContractFactory("Cars");
+    const cars = await Cars.deploy();
+    await cars.waitForDeployment();
     
     // Verify numCars is a function (public getter)
-    ___(cars.numCars).to.be.a("___");
+    expect(cars.numCars).to.be.a("function");
   });
 
   it("should have an addCar function", async function () {
-    const Cars = await ethers.___("Cars");
-    const cars = await Cars.___;
-    await cars.___();
+    const Cars = await ethers.getContractFactory("Cars");
+    const cars = await Cars.deploy();
+    await cars.waitForDeployment();
     
     // Verify addCar function exists
-    ___(cars.addCar).to.be.a("function");
+    expect(cars.addCar).to.be.a("function");
   });
 
 });
